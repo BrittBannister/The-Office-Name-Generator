3d2a2d0bf6450bb33baaa3c0bedf39b8b2bbbc93
Britt <brittbannister1127@icloud.com>
2021-01-13 16:28:49 -0500

    pseudo code laid out. small tuples created for first and last names.

---
+++ names.py
@@ -0,0 +1,16 @@
+import sys
+import random
+
+print('Welcome to The Office Name Picker!\n')
+print('Here is your name:')
+
+# load list of tuples: first and last names
+first = ('Michael', 'Dwight', 'Jim', 'Pam', 'Ryan', 'Andy', 'Robert')
+last = ('Scott', 'Schrute', 'Halpert', 'Beesly',
+        'Howard', 'Bernard', 'California')
+
+# Choose first and last name at random, assign to variable.
+# Print names to screen - highlight name so it stands out.
+# Ask user to quit or play again.
+# If play again: repeat.
+# else: end and exit.
