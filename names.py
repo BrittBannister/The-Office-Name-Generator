1936a04c7e89190bb74deec8ac9e95271c7baa4a
Britt <brittbannister1127@icloud.com>
2021-01-13 16:46:58 -0500

    program correctly selecting first and last names at random, printing to terminal, has option to exit

--- names.py
+++ names.py
@@ -4,13 +4,19 @@ import random
 print('Welcome to The Office Name Picker!\n')
 print('Here is your name:')
 
-# load list of tuples: first and last names
 first = ('Michael', 'Dwight', 'Jim', 'Pam', 'Ryan', 'Andy', 'Robert')
 last = ('Scott', 'Schrute', 'Halpert', 'Beesly',
         'Howard', 'Bernard', 'California')
 
-# Choose first and last name at random, assign to variable.
-# Print names to screen - highlight name so it stands out.
-# Ask user to quit or play again.
-# If play again: repeat.
-# else: end and exit.
+while True:
+    firstname = random.choice(first)
+    lastname = random.choice(last)
+
+    print(firstname, lastname, file=sys.stderr)
+
+    try_again = input('\nTry Again? (Press Enter or x to quit)\n')
+
+    if try_again.lower() == 'x':
+        break
+
+input('\nPress Enter to confirm exit')
